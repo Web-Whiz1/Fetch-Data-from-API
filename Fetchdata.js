f1184d89735e2955b48ac45c96420286ec9103e2
Andrei Damian <andreidamian13@yahoo.com>
2024-10-09 17:45:04 +0300

    Initial commit: Add React scripts

---
+++ Fetchdata.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+
+const FetchData = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) return <p>Loading...</p>;
+
+  return (
+    <div>
+      <h1>Fetched Data</h1>
+      <ul>
+        {data.map((item) => (
+          <li key={item.id}>{item.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default FetchData;
